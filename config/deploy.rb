b1116f9ce1f780af8d648268ce89e584ba75fec6
Melanie Gilman <melanie.gilman@gmail.com>
2012-06-24 18:04:35 -0400

    symlink for secret keys

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,7 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/initializers/secret_keys.rb #{release_path}/config/initializers/secret_keys.rb"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
