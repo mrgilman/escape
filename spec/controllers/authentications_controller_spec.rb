c3502b8804c3514a09501479ff0f60abf664e9dc
Melanie Gilman <melanie.gilman@gmail.com>
2012-06-25 19:07:34 -0400

    controller tests for authentications

--- spec/controllers/authentications_controller_spec.rb
+++ spec/controllers/authentications_controller_spec.rb
@@ -1,31 +1,26 @@
 require 'spec_helper'
 
+OmniAuth.config.test_mode = true
+
 describe AuthenticationsController do
-  # describe "#create" do
-  #   before :each do
-  #     @user = Fabricate(:user)
-  #     login_user
-  #   end
+  describe "#create" do
+    before(:each) do
+      @user = User.create(:email => "a_user@example.com", :password => "hungry")
+      login_user
+    end
 
-  #   it "creates a new authentication" do
-  #     Authentication.count.should == 0
-  #     auth = {:provider => "tripit", :token => "abc123", :secret => "123abc"}
-  #     post :create
-  #     Authentication.count.should == 1
-  #   end
-  # end
+    it "creates a new tripit authentication" do
+      OmniAuth.config.add_mock(:tripit, {:provider => 'tripit', :credentials => { :token => '12345', :secret => 'abcde'} })
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:tripit] 
+      expect { post :create }.to change { @user.authentications.count }.by(1)
+    end
 
-  # describe "add_authentication" do
-  #   before :each do
-  #     @user = Fabricate(:user)
-  #     login_user
-  #   end
+    it "creates a new foursquare authentication" do
+      OmniAuth.config.add_mock(:foursquare, {:provider => 'foursquare', :credentials => { :uid => '12345', :token => 'abcde'} })
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:tripit] 
+      expect { post :create }.to change { @user.authentications.count }.by(1)
+    end
 
-  #   it "creates a new authentication" do
-  #     Authentication.count.should == 0
-  #     auth = {:provider => "tripit", :token => "abc123", :secret => "123abc"}
-  #     add_authentication(auth)
-  #     Authentication.count.should == 1
-  #   end
-  # end
+  end
+
 end
