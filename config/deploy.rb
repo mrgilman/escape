1fb25f37c5629df6e0a0328d9af0a418b5cfa5ec
Melanie Gilman <melanie.gilman@gmail.com>
2012-06-29 20:47:42 -0400

    Fix deploy file again

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ namespace :deploy do
     end
     desc "start resque"
     task command, roles: :app do
-      run("cd #{deploy_to}/current && /usr/bin/env rake resque:worker RAILS_ENV=production")
+      run("cd #{deploy_to}/current && /usr/bin/env rake resque:work RAILS_ENV=production")
       run("cd #{deploy_to}/current && /usr/bin/env rake resque:scheduler RAILS_ENV=production")
     end
   end
