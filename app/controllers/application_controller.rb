47b61731ec5c4371b7159ad06de4e283e29f2dd6
Melanie Gilman <melanie.gilman@gmail.com>
2012-06-30 09:44:15 -0400

    redirect instead of error for cancan access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "You are not authorized to view this page."
+    redirect_to root_url
+  end
+
 end
