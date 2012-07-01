e3b21736b46e91499575ca5f646c034a4abb10cb
Melanie Gilman <melanie.gilman@gmail.com>
2012-06-30 22:03:16 -0400

    styling with twitter

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,10 +1,18 @@
 module TripsHelper
   def foursquare_authentication?
-    trip = Trip.find(params[:id])
-    if trip.user.authentications.where(:provider => "foursquare").empty?
+    if current_user.authentications.where(:provider => "foursquare").empty?
       false
     else
       true
     end
   end
+
+  def twitter_authentication?
+    if current_user.authentications.where(:provider => "twitter").empty?
+      false
+    else
+      true
+    end
+  end
+
 end
